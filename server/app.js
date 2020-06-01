290a49f84e9cd58a1f1adf9bcfcf4989b2cd0182
bimdaya <bimalidayananda@gmail.com>
2020-06-01 21:25:56 +0300

    Fix deployement issues

--- server/app.js
+++ server/app.js
@@ -1,29 +1,29 @@
-// const { setConfig } = require('next/config');
+const { setConfig } = require('next/config');
 
-// const express = require('express');
-// const next = require('next');
-// const { Signale } = require('signale');
+const express = require('express');
+const next = require('next');
+const { Signale } = require('signale');
 
-// const dev = process.env.NODE_ENV !== 'production';
-// const port = process.env.PORT || 3000;
-// const app = next({ dev });
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+const app = next({ dev });
 
-// const handle = app.getRequestHandler();
+const handle = app.getRequestHandler();
 
-// const options = {
-//   scope: 'app server',
-// };
-// const signale = new Signale(options);
+const options = {
+  scope: 'app server',
+};
+const signale = new Signale(options);
 
-// (async () => {
-//   await app.prepare();
-//   const server = express();
+(async () => {
+  await app.prepare();
+  const server = express();
 
-//   //server.use(nextI18NextMiddleware(nextI18next));
-//   server.use('/static', express.static('public/static'));
+  //server.use(nextI18NextMiddleware(nextI18next));
+  server.use('/static', express.static('public/static'));
 
-//   server.get('*', (req, res) => handle(req, res));
+  server.get('*', (req, res) => handle(req, res));
 
-//   await server.listen(port);
-//   signale.success(`<> React Next Boilerplate ready on localhost:${port}`);
-// })();
+  await server.listen(port);
+  signale.success(`<> React Next Boilerplate ready on localhost:${port}`);
+})();
